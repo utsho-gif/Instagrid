26ca07d7120b7a16185088cd545afc02fec00209
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-03-11 04:40:50 +0600

    repetition fixed

--- js/app.js
+++ js/app.js
@@ -146,6 +146,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML = '';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
